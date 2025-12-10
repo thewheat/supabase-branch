0ab598da8bf0eaf4ee07f1e2db0b2ca39cdd6c07
Timothy Lim <tim.lim@supabase.io>
2025-12-10 14:52:43 +0800

    Add test4

---
+++ supabase/migrations/20251210065216_test4.sql
@@ -0,0 +1,7 @@
+create table if not exists test4  (
+        id bigint primary key generated always as identity,
+        name text not null,
+        email text,
+        created_at timestamptz default now()
+        );
+  
